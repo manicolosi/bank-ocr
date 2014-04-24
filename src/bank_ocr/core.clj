8d92e5a5e4f65aac6c17ea1d93eedc7f57eebdeb
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-04-23 19:36:38 -0500

    Uses checksum to check validity

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -33,3 +33,17 @@
   (let [char-set (character-set recognized-numbers)
         parsed-input (separate-numbers 3 3 (take 3 input))]
     (apply str (map char-set parsed-input))))
+
+(defn int-seq
+  [n]
+  (map #(Integer/parseInt (str %1)) (str n)))
+
+(defn valid?
+  "Checksums a bank account number returns a truthy value if it is valid."
+  [number]
+  (letfn [(checksum []
+            (->> (int-seq number)
+                 reverse
+                 (map-indexed (fn [i n] (* (inc i) n)))
+                 (reduce +)))]
+    (= (mod (checksum) 11) 0)))
--- test/bank_ocr/core_test.clj
+++ test/bank_ocr/core_test.clj
@@ -18,3 +18,13 @@
                  "| || || || || || || ||_   |"
                  "|_||_||_||_||_||_||_| _|  |"
                  "                           "]))))
+
+(deftest validity
+  (testing "is valid"
+    (is (valid? "345882865"))
+    (is (valid? "457508000"))
+    (is (valid? "123456789"))
+    (is (valid? "000000051")))
+  (testing "is not valid"
+    (is (not (valid? "664371495")))
+    (is (not (valid? "111111111")))))
